c20765caa8a9d4b22b712a5768a4a995d76f2089
TM <tomaszmuc@gmail.com>
2017-08-26 16:19:17 +0200

    initialising spring boot with spring batch

---
+++ src/main/java/com/tm/finbatch/demo/FinbatchApplication.java
@@ -0,0 +1,12 @@
+package com.tm.finbatch.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinbatchApplication.class, args);
+	}
+}
